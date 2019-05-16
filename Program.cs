ae279f5115e943c247cee910ac925cb9f42628d2
James Dockery <44484428+jamesdockery@users.noreply.github.com>
2019-05-16 12:38:50 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrencyConverter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
